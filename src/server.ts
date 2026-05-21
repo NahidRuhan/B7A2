f3d3b9e58b3794d69431cdaf2375e2a3d34495c6
NahidRuhan <dilal328@gmail.com>
2026-05-21 14:16:36 +0600

    created .env and config and hid all the secrets

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import type { Request, Response } from "express"
 
 import express from "express"
+import config from "./config"
 const app = express()
-const port = 3000
+const port = config.port || 3000
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
